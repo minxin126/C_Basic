695b65a26089c790ac293dda3125ef99dcd015b9
minxin wang <359676107@qq.com>
2019-03-30 16:50:37 +0800

    乱码

--- C++_Basic/乱码.cpp
+++ C++_Basic/乱码.cpp
@@ -1,26 +1,25 @@
-#include <iostream>
-using namespace std; //c++�������ռ�
+#include<iostream>
+using namespace std;//c++的命名空间
 class circle
 {
-  public:
+public:
+
 	double r;
 
 	double pi = 3.1415926;
-<<<<<<< HEAD
-	double area = pi*r*r;
+	double area = pi * r*r;
 
-=======
-	double area = pi * r * r;
->>>>>>> 9758e77c854b79fc47d397c558c4f1811bf9d8e2
 };
 int main()
 {
 	circle pi;
-	cout << "������area" << endl;
+	cout << "请输入area" << endl;
 	cin >> pi.r;
 
-	cout << pi.area << endl; //����
+	cout << pi.area << endl;	//乱码
 
 	system("pause");
 	return 0;
+
+
 }
