6176e564f375ac1bd64a5e6d39f900934525163e
minxin wang <359676107@qq.com>
2019-04-06 23:16:29 +0800

    二级指针

---
+++ C++_Basic/二级指针.cpp
@@ -0,0 +1,36 @@
+#include"iostream"
+using namespace std;
+struct  Teacher
+{
+	char name[64];
+	int age;
+};
+int getTe(struct Teacher **myp)//结构体作为二级指针myp
+{
+	Teacher *p = (Teacher *)malloc(sizeof(Teacher));//p为指针变量
+	if (p=NULL)
+	{
+		return -1;
+	}
+	memset(p, 0, sizeof(Teacher));//全部设置为0
+	p->age = 33;//age
+	*myp = p; 
+	return 0;
+
+}
+//指针的引用而已
+int getTe2(Teacher* &myp) //相当于一个二级指针
+{
+	myp = (Teacher *)malloc(sizeof(Teacher));
+	myp->age = 34;
+
+	return 0;
+}
+
+void main()
+{
+	Teacher *p = NULL;
+	//getTe(&p);
+	getTe2(p);
+	printf("age:%d \n", p->age);
+}
