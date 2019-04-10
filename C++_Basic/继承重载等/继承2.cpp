0ede1ad339a3986086d16899fa9ea766dbcff456
minxin wang <359676107@qq.com>
2019-04-11 00:18:55 +0800

    未完待续

---
+++ C++_Basic/继承重载等/继承2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+//基类
+class SHape
+{
+public:
+	void setWidth(int w)
+	{
+		width = w;
+	}
+	void setHeigth(int w)
+	{
+		height = h;
+	}
+protected:
+	int width;
+	int height;
+};//没写完，之后继续完成
