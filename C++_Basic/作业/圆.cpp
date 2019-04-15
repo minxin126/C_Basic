f824875a2016a79103e2581f4db81b03edad86dc
minxin wang <359676107@qq.com>
2019-04-16 00:57:10 +0800

    未完成，明天首先完成，然后再看课程

--- C++_Basic/作业/圆.cpp
+++ C++_Basic/作业/圆.cpp
@@ -5,23 +5,28 @@ class Point
 public:
 	int p_x;//点的横坐标
 	int p_y;//点的纵坐标
-	float distance;
-	int getX()
+
+	int getX(int p_x)
 	{
 		cin >> p_x;
 		return  p_x;
 	}
-	int getY()
+	int getY(int p_y)
 	{
 		cin >> p_y;
 		return p_y;
 	}
-	int Calculate()
-	{
-		distance = sqrt(p_x*p_x + p_y * p_y);//错误，后期更改
-		return distance;
-	}
+
 };
+Point p1;
+Point p2;
+int Calculate()//计算两点的距离
+{
+	float distance;
+	distance = sqrt((p1.getX(p_x) - p2.getX(p_x)) ^ 2 + (p2.getY(p_y) - p2.getY(p_y) ^ 2);//
+	return distance;
+	//完全独立写，还是很不熟练。得更加独立写。明天继续
+}
 class  Circle
 {
 public:
@@ -37,5 +42,5 @@ int main()
 	p1.getX();
 	cout << "输入纵坐标：" << endl;
 	p1.getY();
-	cout << "两点的距离为：" << p1.Calculate() << endl;
+	cout << "两点的距离为：" << Calculate() << endl;
 }
