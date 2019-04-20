c786d98c42916c0c5744a0ac96a15f2dcb6a0177
minxin wang <359676107@qq.com>
2019-04-21 00:42:32 +0800

    更改

--- C++_Basic/006对象构造和析构专题/demo03拷贝构造函数的四种应用场景01.cpp
+++ C++_Basic/006对象构造和析构专题/demo03拷贝构造函数的四种应用场景01.cpp
@@ -1,58 +1,38 @@
 #include"iostream"
 using namespace std;
-class A
+
+class AA
 {
 public:
-protected:
-private:
-};
-struct C
-{
-};
-class B
-{
-public:
-protected:
-private:
-	A a;
-};
-class Test02
-{
-public:
-	Test02(int _a = 0, int _b = 0)//无参构造函数 默认构造函数
+	AA()//无参构造函数 默认构造函数
 	{
-		p = (char *)malloc(100);
-		strcpy(p, "11111");
-		a = _a;
-		b = _b;
 		cout << "我是构造函数，自动被调用了" << endl;
 	}
-	~Test02()//无参构造函数 默认构造函数
+	AA(const AA &obj)
+	{
+		cout << "我也是构造函数，我是通过另外一个对象obj2，来初始化自己" << endl;
+	}
+	~AA()//无参构造函数 默认构造函数
 	{
 		cout << "我是析构函数，自动被调用了" << endl;
-		if (p != NULL)
-		{
-			free(p);
-		}
 	}
 protected:
 private:
 	int a;
-	int b;
-	char *p;
 };
 //单独搭建一个舞台
 void objPlay()
 {
-	Test02 t1(1, 2);
-	Test02 t2 = (1, 2);
-	Test02   t3 = Test02(3, 4);
-	cout << "展示t1的周期" << endl;
+	AA a1;//变量定义
+	//赋值构造函数的第一个应用场景
+	//我用对象1 初始化 对象2
+	AA a2 = a1;//定义变量并初始化
+	a2 = a1;//用a1来=号给a2,编译器给我提供的浅copy
 }
 void main()
 {
-	//objPlay();
-	printf("A:%d \n", sizeof(A));
-	printf("C:%d \n", sizeof(C));
-	B b1;
+	objPlay();
+	//printf("A:%d \n", sizeof(A));
+	//printf("C:%d \n", sizeof(C));
+	//B b1;
 }
