0fc14d3c4e44bb61711d0db696466462967f75d0
minxin wang <359676107@qq.com>
2019-04-27 00:54:35 +0800

    循环链表

---
+++ C++_Basic/Day20/demo_线性表链式存储.c
@@ -0,0 +1,52 @@
+﻿#include<stdio.h>
+#include "stdlib.h"
+#include "string.h"
+#include "linklist.h"
+
+typedef struct Teacher
+{
+	LinkListNode node;
+	char name[64];
+	int age;
+}Teacher;
+
+int main()
+{
+	Teacher		t1, t2, t3;
+	int			length, i = 0;
+
+	LinkList		*list = NULL;
+	t1.age = 31;
+	t2.age = 32;
+	t3.age = 33;
+
+	list = LinkList_Create();
+
+	length = LinkList_Length(list);
+
+	LinkList_Insert(list, (LinkListNode *)&t1, LinkList_Length(list));
+	LinkList_Insert(list, (LinkListNode *)&t2, LinkList_Length(list));
+	LinkList_Insert(list, (LinkListNode *)&t3, LinkList_Length(list));
+
+	//±éÀúÁ´±í
+	for (i = 0; i < LinkList_Length(list); i++)
+	{
+		Teacher *tmp = (Teacher *)LinkList_Get(list, i);
+		if (tmp != NULL)
+		{
+			printf("age:%d ", tmp->age);
+		}
+	}
+
+	while (LinkList_Length(list) > 0)
+	{
+		Teacher *tmp = (Teacher *)LinkList_Delete(list, 0);
+		if (tmp != NULL)
+		{
+			printf("age:%d ", tmp->age);
+		}
+	}
+	LinkList_Destroy(list);
+
+	return 0;
+}
