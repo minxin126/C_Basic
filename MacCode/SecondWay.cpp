4bc5bed9c8fa81090306fde1b70b982b8cde0400
minxwang <wangminxin6@126.com>
2020-06-13 21:25:21 +0800

    ss

---
+++ MacCode/SecondWay.cpp
@@ -0,0 +1,5 @@
+//
+// Created by minx wang on 2020/6/13.
+//
+
+#include "SecondWay.h"
---
+++ MacCode/SecondWay.h
@@ -0,0 +1,14 @@
+//
+// Created by minx wang on 2020/6/13.
+//
+
+#ifndef MACCODE_SECONDWAY_H
+#define MACCODE_SECONDWAY_H
+
+
+class SecondWay {
+
+};
+
+
+#endif //MACCODE_SECONDWAY_H
